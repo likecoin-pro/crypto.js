ed5fa4937a80e4475560492bdd5068e91662ba1b
Rifat Jumagulov <jum.rifm@gmail.com>
2018-05-16 22:57:16 +0300

    Added karma-spec-reporter to print spec timing

--- karma.conf.js
+++ karma.conf.js
@@ -32,7 +32,13 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['spec'],
+
+        specReporter: {
+            maxLogLines: 5,
+            showSpecTiming: true,
+            failFast: true
+        },
 
 
         // web server port
